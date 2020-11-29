169366ed42e9be7cf76149c32aaecb3337ee0d82
soonil <abrahamim365@gmail.com>
2020-11-29 09:51:07 +0900

    2020 11 29

--- beginner/src/beginner/Var/var4.java
+++ beginner/src/beginner/Var/var4.java
@@ -3,8 +3,27 @@ package beginner.Var;
 public class var4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n1 = 10; //리터럴 Literal (10, 20, 10.1)
+		int n2 = 20;
+		int n3 = n1 + n2;
+		
+		System.out.println(n3);
+		
+		float f1 = 10.1F;
+		double f2 = 10.1D;
+		
+		int n4 = (int)f2;
+		System.out.println(n4);
+		
+		//-----------------------------------------
+		
+		System.out.println("-----------------------");
+		
+		int num1 = 10;
+		int num2 = 3;
+		float result = (float)num1 / num2;
+		
+		System.out.println(result);
 	}
 
 }
---
+++ beginner/src/beginner/Var/var5.java
@@ -0,0 +1,21 @@
+package beginner.Var;
+
+public class var5 {
+
+	public static void main(String[] args) {
+		//오버플로우, 언더플로우
+		
+		byte d1 = (byte)256;
+		System.out.println(d1);
+		
+		byte d2 = (byte)-129;
+		System.out.println(d2);
+		
+		long d3 = 1010101010010111233L;
+		int d4 = (int)d3;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(d4);
+
+	}
+
+}
