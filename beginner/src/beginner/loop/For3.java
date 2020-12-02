5e810f04563b808a6d8205c02d474b261fa3292d
soonil <abrahamim365@gmail.com>
2020-12-02 21:22:26 +0900

    2020 12 02

---
+++ beginner/src/beginner/loop/For3.java
@@ -0,0 +1,15 @@
+package beginner.loop;
+
+public class For3 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 24; i++) {
+			for (int z = 0; z <=59; z++) {
+				System.out.printf("%d : %d \n",i,z);
+			}
+			System.out.println("땡!");
+		}
+
+	}
+
+}
