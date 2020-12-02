cda92262e46b817f6e801855bd9b1aca4f3f7460
soonil <abrahamim365@gmail.com>
2020-12-02 21:22:10 +0900

    2020 12 02

---
+++ beginner/src/beginner/loop/For2.java
@@ -0,0 +1,13 @@
+package beginner.loop;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		int dan = 3;
+		for (int i = 67; i < 76; i++) {
+			System.out.printf("%d x %d = %d\n",dan,i-66,dan*(i-66));
+		}
+
+	}
+
+}
