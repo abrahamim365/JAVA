59d1262d51508afa94288db0cf20ffccbe60661f
soonil <abrahamim365@gmail.com>
2020-12-03 19:49:26 +0900

    2020 12 03

---
+++ beginner/src/beginner/loop/For5.java
@@ -0,0 +1,17 @@
+package beginner.loop;
+
+public class For5 {
+
+	public static void main(String[] args) {
+		int star = 5;
+		
+		for (int i = 0; i < star; i++) {
+			for (int j = 0; j < star; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
