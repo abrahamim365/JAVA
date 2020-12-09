a1a2c90dd28020b8c8d15e6143c65f9bd05aa688
soonil <abrahamim365@gmail.com>
2020-12-09 11:34:39 +0900

    2020 12 09

---
+++ Practice/Practice/src/Test/Obj/CardTest.java
@@ -0,0 +1,33 @@
+package Test.Obj;
+
+public class CardTest {
+
+	public static void main(String[] args) {
+		
+	//클래스변수 width와 height를 출력하라
+		
+	//참조변수를 활용해서 인스턴스변수를 선언하고 Heart,7 과 Spade,4로 값을 변경해라
+
+	System.out.println("c1은" + c1.kind + ", " +c1.number +"이며, 크기는 (" + c1.width + ", "+
+											c1.height + ")" );
+	System.out.println("c2은" + c2.kind + ", " +c2.number +"이며, 크기는 (" + c2.width + ", "+
+											c2.height + ")" );
+	System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
+	 //클래스변수의 값을 50,80으로 변경해라
+	
+	System.out.println("c1은" + c1.kind + ", " +c1.number +"이며, 크기는 (" + c1.width + ", "+
+											c1.height + ")" );
+	System.out.println("c2은" + c2.kind + ", " +c2.number +"이며, 크기는 (" + c2.width + ", "+
+											c2.height + ")" );
+		
+	}
+}
+	
+	
+
+//클래스 변수 width=100,height=250으로 설정하고 인스턴스변수 kind,number를 선언하라.
+class Card { 
+	
+}
+
+
