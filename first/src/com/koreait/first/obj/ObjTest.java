ea8c4038217e61f7a775e701287cf42c374fee29
soonil <abrahamim365@gmail.com>
2020-12-14 21:30:38 +0900

    2020 12 14

---
+++ first/src/com/koreait/first/obj/ObjTest.java
@@ -0,0 +1,33 @@
+package com.koreait.first.obj;
+
+public class ObjTest {
+	public static void main(String[] args) {
+		int num = 14;
+			NumberBox nbx = new NumberBox(); //대구 주택
+			nbx.num = 10;
+			
+			changeVal(num);
+			changeVal(nbx);
+			
+			System.out.println("num:"+num);
+			System.out.println("nbx.num:"+nbx.num);
+		}
+	
+	public static void changeVal(int num) {
+		num=13;	
+		System.out.println(num);
+	}
+	
+	public static void changeVal(NumberBox pNbx) {
+			pNbx.num = 20;
+	}
+}
+
+class NumberBox {
+	int num;
+}
+
+
+
+
+
