d5e8a19e9104ba1f77357b15a8b684a78b123135
soonil <abrahamim365@gmail.com>
2020-12-19 16:47:13 +0900

    2020 12 19

---
+++ first/src/com/koreait/first/obj/StaticTest2.java
@@ -0,0 +1,36 @@
+package com.koreait.first.obj;
+
+public class StaticTest2 {
+
+	public static void main(String[] args) {
+		//n1 = 22, n2 = 55;
+		//인스턴스 객체를 활용하여 계산
+		InstCalc ic = new InstCalc();
+		ic.n1=22;
+		ic.n2=55;
+	
+		System.out.println(ic.sum());
+		
+		//static 메소드를 활용하여 계산
+		//System.out.println("sum : " + StatCalc.sum(22, 55));
+		StatCalc sc = new StatCalc();
+		System.out.println("sum : "+sc.sum(22,55));
+		
+	}
+}
+
+class StatCalc {
+	int sum(int x, int y) {
+		return x + y;
+	}
+	
+}
+
+class InstCalc {
+	int n1;
+	int n2;
+	int n3;
+	int sum() {
+		return n1; 
+	}
+}
