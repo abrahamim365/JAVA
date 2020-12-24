88403cecdaec798480c5e64b3454af3ef0074674
soonil <abrahamim365@gmail.com>
2020-12-24 21:26:31 +0900

    2020 12 24

---
+++ first/src/com/koreait/first/obj2/InheriTest2.java
@@ -0,0 +1,13 @@
+package com.koreait.first.obj2;
+
+public class InheriTest2 {
+
+	public static void main(String[] args) {
+		//ClassC cC = new ClassC("김남길", 22);
+		ClassC cC = new ClassC("",0);
+		cC.setName("김남길");
+		cC.setAge(22);
+		cC.print();
+	}
+
+}
