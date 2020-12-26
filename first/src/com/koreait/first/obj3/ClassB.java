ba8d7090cbb383011a793bfcacffcbd9745f71d3
soonil <abrahamim365@gmail.com>
2020-12-26 20:54:50 +0900

    2020 12 26

---
+++ first/src/com/koreait/first/obj3/ClassB.java
@@ -0,0 +1,18 @@
+package com.koreait.first.obj3;
+
+public class ClassB extends ClassA {
+	public ClassB(String name, int age) {
+		super(name,age);
+	}
+	
+	@Override
+	public void print() {
+	System.out.println("B: "+getName()+"/"+getAge());
+	}
+	
+	public void drive() {
+		System.out.println("driving!!");
+	}
+}
+
+
