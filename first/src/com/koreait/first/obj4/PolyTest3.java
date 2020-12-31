7418174a5cdb53d12e9d0fa8c7f12ce026abea4d
soonil <abrahamim365@gmail.com>
2020-12-31 12:37:13 +0900

    2020 12 31

---
+++ first/src/com/koreait/first/obj4/PolyTest3.java
@@ -0,0 +1,28 @@
+package com.koreait.first.obj4;
+
+public class PolyTest3 {
+	public static void main(String[] args) {
+			Dog dog = new Dog();
+			Cat cat = new Cat();
+			Bird bird = new Bird();
+			
+			Animal ani = cat;
+			System.out.println(ani instanceof Dog);
+			polyCry(dog);
+			polyCry(cat);
+			polyCry(bird);
+	}
+	
+	public static void polyCry(Animal ani) {
+		ani.crying();
+		if (ani instanceof Dog) {
+			//Dog dog = (Dog)ani;
+			//dog.shakeTail();
+			((Dog)ani).shakeTail();
+		}
+	}
+}
+
+
+
+
