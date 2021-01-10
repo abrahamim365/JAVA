bfe25cd3637cb9cbab325613f7acbd96174ee3d5
soonil <abrahamim365@gmail.com>
2021-01-10 14:58:34 +0900

    2021 1 10

---
+++ Practice/Practice/src/Abstract/AnimalExample.java
@@ -0,0 +1,36 @@
+package Abstract;
+
+public class AnimalExample {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		
+		dog.sound();
+		cat.sound();
+		
+		Animal animal = null;
+		
+		animal = new Dog(); //자동 타입변환
+		animal.sound(); //Dog에 구현된 Sound()메서드 실행
+		
+		animal = new Cat(); //자동 타입변환
+		animal.sound(); //Cat에 구현된 Sound()메서드 실행
+		
+		animalSound(new Dog()); //자동 타입변환 (매개변수도 가능)
+		animalSound(new Cat()); //자동 타입변환 (매개변수도 가능)
+		
+	}
+	//자동 타입변환 : 추상클래스 타입 변수는 추상클래스를 상속받은 실체클래스의 타입으로 자동 타입변환이 변환된다.
+
+	private static void animalSound(Dog dog) {
+		
+		
+	}
+
+	private static void animalSound(Cat cat) {
+		
+		
+	}
+
+}
